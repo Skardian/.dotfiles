6909117df3786234a44469e84391ddcb8eabf0e2
Pepe Padial <jose.carlos.padial@gmail.com>
2015-02-09 22:08:16 +0100

    Updated install script (Should work now)

--- install_configs.sh
+++ install_configs.sh
@@ -3,7 +3,9 @@ mv ~/.vimrc .old/.vimrc
 mv ~/.gitconfig .old/.gitconfig
 mv ~/.vim .old/vim
 
-ln -sf ./.bashrc ~/.bashrc
-ln -sf ./.vimrc ~/.vimrc
-ln -sf ./.gitconfig ~/.gitconfig
-ln -sf ./.vim ~/.vim
+ln -sf ~/configs/.bashrc ~/.bashrc
+ln -sf ~/configs/.vimrc ~/.vimrc
+ln -sf ~/configs/.gitconfig ~/.gitconfig
+ln -sf ~/configs/.vim ~/.vim
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
