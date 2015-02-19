5368d55aa8edb00e96673276151f751a2943e537
Pepe Padial <jose.carlos.padial@gmail.com>
2015-02-19 21:11:33 +0100

    Changed install script due to repo rename

--- install_configs.sh
+++ install_configs.sh
@@ -1,11 +1,13 @@
+mkdir -p ~/.dotfiles/.old
+
 mv ~/.bashrc  .old/.bashrc
 mv ~/.vimrc .old/.vimrc
 mv ~/.gitconfig .old/.gitconfig
 mv ~/.vim .old/vim
 
-ln -sf ~/configs/.bashrc ~/.bashrc
-ln -sf ~/configs/.vimrc ~/.vimrc
-ln -sf ~/configs/.gitconfig ~/.gitconfig
-ln -sf ~/configs/.vim ~/.vim
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/.vim ~/.vim
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
