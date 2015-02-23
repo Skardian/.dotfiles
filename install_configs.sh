e8e933ccd27711afe40ad639462485c568240802
jcpadial <jose.carlos.padial@gmail.com>
2015-02-23 12:47:38 +0100

    Added DejaVu patched font

--- install_configs.sh
+++ install_configs.sh
@@ -5,11 +5,13 @@ mv -f ~/.vimrc .old/.vimrc
 mv -f ~/.gitconfig .old/.gitconfig
 mv -f ~/.vim .old/.vim
 mv -f ~/.alias .old/.alias
+mv -f ~/.fonts .old/.fonts
 
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.vim ~/.vim
 ln -sf ~/.dotfiles/.alias ~/.alias
+ln -sf ~/.dotfiles/.fonts ~/.fonts
 
 # git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
