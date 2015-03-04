d5375971ad953873172c9cacddc239f3c957474d
jcpadial <jose.carlos.padial@gmail.com>
2015-03-04 15:02:40 +0100

    Added initial tmux config

--- install_configs.sh
+++ install_configs.sh
@@ -6,6 +6,7 @@ mv -f ~/.gitconfig .old/.gitconfig
 mv -f ~/.vim .old/.vim
 mv -f ~/.alias .old/.alias
 mv -f ~/.fonts .old/.fonts
+mv -f ~/.tmux.conf .old/.tmux.conf
 
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
@@ -13,5 +14,6 @@ ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.vim ~/.vim
 ln -sf ~/.dotfiles/.alias ~/.alias
 ln -sf ~/.dotfiles/.fonts ~/.fonts
+ln -sf ~/.tmux.conf ~/.tmux.conf
 
 # git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
