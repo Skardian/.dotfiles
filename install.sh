ab10d5cb71d860c86725cd8a3c7a6a9ec926044e
jcpadial <jose.carlos.padial@gmail.com>
2015-05-04 12:09:19 +0200

    Update install script and readme

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# Files included
+FILES=".alias .bashrc .fonts .gitconfig .tmux.conf .vim .vimrc .zshrc"
+
+mkdir -p ~/.dotfiles/.old
+
+for f in $FILES; do
+    # Remove backup and old files
+    if [ -e ~/$f ]; then
+        rm -rf .old/$f
+        mv -f ~/$f .old/$f
+    fi
+    # Link to the corresponding file
+    ln -sf ~/.dotfiles/$f ~/$f
+done
+
+# Clone Vundle and install vim scripts
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +BundleInstall +qa
+
