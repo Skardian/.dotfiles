a4b32a76f6e428e50e9e32b7db4bad2b25f34294
Pepe Padial <jose.carlos.padial@gmail.com>
2015-05-19 09:58:38 +0200

    Rename vim folder

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env sh
 
 # Files included
-FILES="alias bashrc fonts gitconfig gitignore tmux.conf vim vimrc vrapper zshrc"
-
+FILES="alias bashrc gitconfig gitignore tmux.conf vimrc vrapper zshrc"
 for f in $FILES; do
     # Link to the corresponding file
     ln -sf ~/.dotfiles/$f ~/.$f
 done
 
+# Files included
+FOLDERS="fonts vim"
+for f in $FOLDERS; do
+    # Link to the corresponding file
+    ln -sf ~/.dotfiles/$f/ ~/.$f/
+done
+
 # Clone Vundle and install vim scripts
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +BundleInstall +qa
+if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +BundleInstall +qa
+fi
 
