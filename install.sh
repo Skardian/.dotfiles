e871fa8c64f82797ba1952831f0a9ba1bec6b409
Pepe Padial <jose.carlos.padial@gmail.com>
2015-06-20 21:37:09 +0200

    Update install.sh script

--- install.sh
+++ install.sh
@@ -9,10 +9,8 @@ done
 
 # Files included
 FOLDERS="fonts vim"
-for f in $FOLDERS; do
-    # Link to the corresponding file
-    ln -sf ~/.dotfiles/$f/ ~/$f/
-done
+ln -sf ~/.dotfiles/fonts ~/.fonts
+ln -sf ~/.dotfiles/vi ~/.fonts
 
 # Clone Vundle and install vim scripts
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
