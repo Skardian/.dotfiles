cac4a7caa824254ed4664502a11f12fa1177da49
Pepe Padial <jose.carlos.padial@gmail.com>
2016-04-22 11:53:33 +0200

    [SH] Add custom 'fo' function

---
+++ zsh/functions.zsh
@@ -0,0 +1,14 @@
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+# Source: https://github.com/atweiden/fzf-extras
+#   - CTRL-O to open with `xdg-open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+#   - Exit if there's no match (--exit-0)
+fo() {
+  local out file key
+  out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && xdg-open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
